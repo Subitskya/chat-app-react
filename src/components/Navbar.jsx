f51e6085141df6aa7867228a00c2822886cb7f62
Subit Shakya <subitskya.ss@gmail.com>
2024-07-15 10:32:07 +0545

    added login and logout components

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import {auth} from "../firebase";
 import {useAuthState} from "react-firebase-hooks/auth";
 import SignIn from './SignIn';
+import LogOut from './LogOut';
 
 const style = {
     nav: `bg-gray-800 h-20 flex justify-between items-center p-4`,
@@ -10,10 +11,11 @@ const style = {
 }
 const Navbar = () => {
     const [user] = useAuthState(auth);
+    console.log(user)
   return (
     <div className={style.nav}>
         <h1 className={style.heading}>Chat App</h1>
-        <SignIn />
+        {user ? <LogOut /> : <SignIn /> }
     </div>
   )
 }
--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,8 +1,27 @@
+import {GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import React from 'react'
+import GoogleButton from 'react-google-button'
+import { auth } from '../firebase'
+
+const style = {
+  wrapper: `flex:justify-center`
+}
+const googleSignIn = () => {
+  const provider = new GoogleAuthProvider();
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      console.log("Sign-in successful:", result.user);
+    })
+    .catch((error) => {
+      console.error("Error during sign-in:", error);
+    });
+};
 
 const SignIn = () => {
   return (
-    <div>SignIn</div>
+    <div className={style.wrapper}>
+      <GoogleButton onClick={googleSignIn}/>
+    </div>
   )
 }
 
