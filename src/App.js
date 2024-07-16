9eb17f66315105e7cc109644c44c203768f0b91f
Subit Shakya <subitskya.ss@gmail.com>
2024-07-16 13:38:34 +0545

    add chat, message, sendmessage components with firstore db

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 
 import {auth} from "./firebase";
 import {useAuthState} from "react-firebase-hooks/auth";
+import Chat from "./components/Chat";
 
 const style ={
   appContainer: `max-w-[728px] mx-auto text-center`,
@@ -16,6 +17,8 @@ function App() {
         {/* Navbar */}
         <Navbar />
         {/* Chat component */}
+        {user ? <Chat/> : null}
+        {/* <Chat /> */}
       </section>
     </div>
   );
